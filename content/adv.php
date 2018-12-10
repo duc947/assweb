18e6048ae1208c5e74e710fb49e9ce88b632cf23
Vũ Ngọc Đức <destinywjnd@gmail.com>
2018-12-10 12:37:42 +0700

    advert_content

---
+++ content/adv.php
@@ -0,0 +1,26 @@
+<div class="sidebar col-md-3" style="overflow: visible; height: 1px;">
+    <div class="ad">
+        <div class="ad-title">
+            <h2>Vinfast</h2>
+        </div>
+        <div class="ad-poster">
+            <img src="../images/ad/1.jpg" alt="">
+        </div>
+    </div>
+    <div class="ad">
+        <div class="ad-title">
+            <h2>Nike</h2>
+        </div>
+        <div class="ad-poster">
+            <img src="../images/ad/2.jpg" alt="">
+        </div>
+    </div>
+    <div class="ad">
+        <div class="ad-title">
+            <h2>Pepsi</h2>
+        </div>
+        <div class="ad-poster">
+            <img src="../images/ad/3.jpg" alt="">
+        </div>
+    </div>
+</div>
