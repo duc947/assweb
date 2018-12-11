3f217eeb9a96f86f14f8651a67bbcb4b63e3221a
Vũ Ngọc Đức <destinywjnd@gmail.com>
2018-12-12 00:40:25 +0700

    fix_form

--- content/login.php
+++ content/login.php
@@ -5,14 +5,14 @@
 				<!-- begin phần của you nội dung như đã bàn: đăng kí...-->
 			<form class="form-signin" action="../backend/login.php" method="POST">
 			    <h3 class="form-signin-heading" style="color: #573131">Đăng nhập tài khoản</h3>
-			    <input type="email" id="inputEmail" class="form-control" placeholder="Địa chỉ email" name="email" required autofocus>
-			    <input type="password" id="inputPassword" class="form-control" placeholder="Password" name="password" required autofocus>
-			    <div class="checkbox">
+			    <input type="email" id="inputEmail" class="form-control mt-3" placeholder="Địa chỉ email" name="email" required autofocus>
+			    <input type="password" id="inputPassword" class="form-control mt-3" placeholder="Password" name="password" required autofocus>
+			    <div class="checkbox mt-3">
 					<label>
 					    <input type="checkbox" value="remember-me"> Nhớ mật khẩu
 					</label>
 				</div>
-				<button class="btn btn-lg btn-info btn-block" type="submit">Đăng nhập</button>
+				<button class="btn btn-lg btn-info btn-block mt-3" type="submit">Đăng nhập</button>
 			</form>
 				<!-- the end nội dung của you-->
 		</div>
--- content/reg.php
+++ content/reg.php
@@ -6,15 +6,15 @@
         		
     			<h3 class="form-signin-heading" style="color: #573131">Đăng ký tài khoản</h3>
 		        
-                <input type="text" placeholder="Tên của bạn" class="form-control" id="form-first-name" name="nameInput" required autofocus>
+                <input type="text" placeholder="Tên của bạn" class="form-control mt-3" id="form-first-name" name="nameInput" required autofocus>
 		        
-		        <input type="email" id="inputEmail" class="form-control" placeholder="Địa chỉ email" name="emailInput" required autofocus>
+		        <input type="email" id="inputEmail" class="form-control mt-3" placeholder="Địa chỉ email" name="emailInput" required autofocus>
 		        
-		        <input type="password" id="inputPassword" class="form-control" placeholder="Mật khẩu" name="passInput" required autofocus>
+		        <input type="password" id="inputPassword" class="form-control mt-3" placeholder="Mật khẩu" name="passInput" required autofocus>
 		       
-		        <input type="password" id="inputPassword" class="form-control" placeholder="Xác nhận mật khẩu" name="pass2Input" required autofocus>
+		        <input type="password" id="inputPassword" class="form-control mt-3" placeholder="Xác nhận mật khẩu" name="pass2Input" required autofocus>
 		        
-		     	<button class="btn btn-lg btn-info btn-block" type="submit">Đăng ký</a></button>
+		     	<button class="btn btn-lg btn-info btn-block mt-3" type="submit">Đăng ký</a></button>
 			 </form>
 			<!-- the end nội dung của you-->
 		</div>
